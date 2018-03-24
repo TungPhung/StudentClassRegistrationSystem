d67a05e5d001e873da2f0d6652f9850f359ed362
Tung Phung <ttp2d@virginia.edu>
2018-03-24 02:51:29 -0400

    began work on main registration system method

--- RegistrationSystem.java
+++ RegistrationSystem.java
@@ -1,1 +1,23 @@
+/*
+ * Author - Tung Phung
+ * 
+ * Simulated Registration System for class enrollment consisting of student and course objects
+ * 
+ * 
+ * Date - March 23, 2018
+ */
+package RegistrationSystem;
+
+//Imports
+import CourseDB.SimulatedCourseDatabase;
+import StudentDB.SimulatedStudentDatabase;
+import Course;
+import Student;
 import java.util.*;
+java.util.Scanner input = new Scanner( System.in);
+
+public class Registration System {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Course Registration System.");
+    }
+}
