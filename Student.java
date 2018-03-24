68b0cb6a18e7473e7f00bde35c3b3e419fe16985
Tung Phung <ttp2d@virginia.edu>
2018-03-23 23:08:10 -0400

    Updated Student Class

--- Student.java
+++ Student.java
@@ -43,26 +43,56 @@ public class Student {
     }
     
     //Change a student's ID
-    public void changeID (int id) {
-        this.id = id
+    public void changeID (int newid) {
+        if (this.id == newid) {
+            System.out.println("Unable to change ID - new ID is same as old ID");
+        } else {
+            String oldid = this.id;
+            this.id = newid;
+            System.out.println("ID changed from " + oldid+ " to " + newid);
+        }   
     }
 
-    //Change a student's name
-    public void changeName (String name) {
-        this.name = name;
+    //Change a student's name if it is different from old name
+    public void changeName (String newname) {
+        if (this.name.equals(newname)) {
+            System.out.println("Unable to change name - new name is same as old name");
+        } else {
+            String oldname = this.name;
+            this.name = newname;
+            System.out.println("Name changed from " + oldname + " to " + newname);
+        }   
     }
 
-    //Change a student's major
-    public void changeMajor (String major) {
-        this.major = major;
+    //Change a student's major if it is different from old major
+    public void changeMajor (String newmajor) {
+        if (this.major.equals(newmajor)) {
+            System.out.println("Unable to change major - new major is same as old major");
+        } else {
+            String oldmajor = this.major;
+            this.major = newmajor;
+            System.out.println("Major changed from " + oldmajor + " to " + newmajor);
+        }    
     }
 
     //Add a course to a student's current courses
     public void addCourse (String course) {
-        this.previousClasses.add("course");
+        if (this.previousClasses.contains("course") == true) {
+            System.out.println("Unable to add course - student already enrolled")
+        } else {
+            this.previousClasses.add("course");
+            System.out.println("Course added");
+        }
+        
     }
 
-    public void 
+    //Transfer all current courses to previous courses and empty current courses
+    public void endSemester {
+        for (Iterator<String> iterator = this.currentClasses.iterator(); iterator.hasNext();) {
+            this.previousClasses.add(iterator.next);
+            this.previousClasses.remove(iterator.next);
+        }
+    }
     
     
         
